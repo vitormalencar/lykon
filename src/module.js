0836e981897ec5f20bde7c85baaaf88e3e6870fc
vitormalencar <vitor.maia.alencar@gmail.com>
2020-04-03 00:01:05 +0200

    fixing default export

--- src/module.js
+++ src/module.js
@@ -1,18 +1,16 @@
-import { registerReducer } from './store';
+import { registerReducer } from "./store";
 
 let modules = {};
 
-const module = (store) => (name, moduleProvider) => {
-  if (modules.hasOwnProperty(name)) {
-    return Promise.resolve(modules[name]);
-  }
-  else {
-    return moduleProvider.then(mod => {
-      registerReducer(store, name, mod.reducer);
-      return mod;
-    });
-  }
+export default function module(store) {
+  return (name, moduleProvider) => {
+    if (modules.hasOwnProperty(name)) {
+      return Promise.resolve(modules[name]);
+    } else {
+      return moduleProvider.then(mod => {
+        registerReducer(store, name, mod.reducer);
+        return mod;
+      });
+    }
+  };
 }
-
-
-export default module
